0f6c000c2c8929c4513bd19aa1106cf99a03ea01
Pedro <pjolid@gmail.com>
2024-11-11 09:21:57 +0100

    TopBar

---
+++ app/src/main/java/com/example/proyectoui/scaffold/MyTopBar.kt
@@ -0,0 +1,54 @@
+package com.example.proyectoui.scaffold
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AcUnit
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Android
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyTopBar() {
+    TopAppBar(
+        title = {
+            Row {
+                Text("Proyecto UI")
+                Spacer(Modifier.width(8.dp))
+                Icon(imageVector = Icons.Default.Android, contentDescription = null)
+            }
+        },
+        navigationIcon = {
+            IconButton(onClick = {}) {
+                Icon(imageVector = Icons.Default.ArrowBack, contentDescription = null)
+            }
+        },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+            IconButton(onClick = {}) {
+                Icon(imageVector = Icons.Default.AcUnit, contentDescription = null)
+            }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = Color.Blue,
+            titleContentColor = Color.White,
+            navigationIconContentColor = Color.White,
+            actionIconContentColor = Color.White
+        )
+    )
+}
--- app/src/main/java/com/example/proyectoui/ui/screen/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/proyectoui/ui/screen/homeScreen/HomeScreen.kt
@@ -1,14 +1,29 @@
 package com.example.proyectoui.ui.screen.homeScreen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Android
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBarDefaults.containerColor
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,14 +31,20 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.example.proyectoui.R
 import com.example.proyectoui.scaffold.MyFloatingActionButton
+import com.example.proyectoui.scaffold.MyTopBar
 
 @Composable
 fun HomeScreen(navigateToDetalle: (String) -> Unit) {
     var nombre by remember { mutableStateOf("") }
-    Scaffold (
-        floatingActionButton = { MyFloatingActionButton(navigateToDetalle, nombre) }
-    ){ paddingValue ->
+    Scaffold(
+        floatingActionButton = { MyFloatingActionButton(navigateToDetalle, nombre) },
+        topBar = { MyTopBar() }
+    ) { paddingValue ->
         Column(
             modifier = Modifier
                 .fillMaxSize()
@@ -45,5 +66,3 @@ fun HomeScreen(navigateToDetalle: (String) -> Unit) {
         }
     }
 }
-
-
